96a97aa27e014b6e8e97c8bcfa6ec1e8ebb70bd0
gurtejveer <85623827+gurtejveer@users.noreply.github.com>
2021-09-29 08:46:01 +0530

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,24 @@
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputDir ={x:0,y:01}
+    move.mp3.play();
+    switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp")
+            break;
+        
+     case "ArrowDown":
+    console.log("ArrowDown")
+    break;
+    case "ArrowLeft":
+        console.log("ArrowLeft")
+        break;
+        case "ArrowRight":
+            console.log("ArrowRight")
+            break;
+     default:
+         break;   
+    }
+})
+ 
+ 
